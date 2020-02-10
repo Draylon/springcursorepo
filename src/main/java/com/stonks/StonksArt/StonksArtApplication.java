01f0a08c0eb0edb6885058a301f2acdc86bacea6
Draylon <draylid@gmail.com>
2020-02-10 19:56:52 -0300

    2

--- src/main/java/com/stonks/StonksArt/StonksArtApplication.java
+++ src/main/java/com/stonks/StonksArt/StonksArtApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StonksArtApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(StonksArtApplication.class, args);
+		System.out.println("STONKS___________-");
 	}
 
 }
