25a6680988f27e4edcf296ffb4e65a2698847a06
Draylon <draylid@gmail.com>
2020-02-10 19:46:47 -0300

    1st commit

---
+++ src/main/java/com/stonks/StonksArt/StonksArtApplication.java
@@ -0,0 +1,13 @@
+package com.stonks.StonksArt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StonksArtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StonksArtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stonks/StonksArt/StonksArtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stonks.StonksArt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StonksArtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
